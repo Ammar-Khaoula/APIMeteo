45f06f75d3f29b6d8590d2d19359a381c7ba19e4
ammar khaoula <khaoulabha1991@gmail.com>
2022-03-05 21:21:43 +0100

    modify js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const APIKey = "14adb44fdda9d21d1ca1df09d3f9c0b7";
 
 let getApiMeteo = function (city) {
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric&lang=fr`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric&lang=fr`;
 
 fetch(url)
     .then((response) => response.json())
